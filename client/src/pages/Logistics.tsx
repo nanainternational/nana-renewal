6fdd4ca7f2590fef11b0b27a38d97fe8314d9a16
nanainternational <120464980+nanainternational@users.noreply.github.com>
2026-08-14 15:18:19 +0900

    Update Logistics.tsx

--- client/src/pages/Logistics.tsx
+++ client/src/pages/Logistics.tsx
@@ -701,13 +701,13 @@ export default function Logistics() {
                   추가 비용 기준
                 </div>
                 <p className="diff-desc">
-                  OPP 교체 <strong>+100원</strong>
+                  OPP 교체 <strong>0원</strong>
                   <br />
-                  택갈이 <strong>+100원</strong>
+                  택갈이 <strong>0원</strong>
                   <br />
                   반품 출고비 기준 <strong>+300원</strong>
                   <br />
-                  합배송 <strong>+500원</strong>
+                  합배송 <strong>0원</strong>
                 </p>
               </div>
             </div>
